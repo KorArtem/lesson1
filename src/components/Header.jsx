cbc258d59dd6081b110ef605327b3e956626eb53
unknown <koriaulin_as@icloud.com>
2022-10-31 23:40:54 +0300

    Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css" 
+
+const Header = () =>{
+    return(
+        <header className="header">
+            <img src="https://www.imgacademy.com/sites/default/files/2021-06/ascender1_10.png"/>
+        </header>
+        
+    )
+}
+
+export default Header; 
