66057cf14e8fe08b303ddb6230d0f22480d9df43
unknown <koriaulin_as@icloud.com>
2022-10-31 23:57:30 +0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="">
       <header className="">
         <Header/>
+        <Navbar/>
       </header>
     </div>
   );
